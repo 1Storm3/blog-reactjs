ab9e73bd7c93ce91dcdfab8befebfb2b09ddc323
storm <zemskoff2013@yandex.ru>
2024-03-17 12:51:34 +0300

    3 fix

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -30,7 +30,7 @@ const LoginPage: FunctionComponent = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("http://localhost:13000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
